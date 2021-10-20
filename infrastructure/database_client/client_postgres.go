9f7b179afc1853aca52cf761400ff3322cc7bea4
jesus lozano <jalozanot@gmail.com>
2021-10-19 21:37:50 -0500

    prueba

--- infrastructure/database_client/client_postgres.go
+++ infrastructure/database_client/client_postgres.go
@@ -4,24 +4,16 @@ import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"log"
+	"os"
 	"yofio/domain/model"
 )
 
 func GetConnectionPostgres() *gorm.DB {
 
+	connection := os.Getenv("DATABASE_URL")
 
-	host := "ec2-54-92-230-7.compute-1.amazonaws.com"
-	port := "5432"
-	dbname := "d12sdavvqmev9l"
-	user := "mwhdjvrlkijbnh"
-	password := "fba5a673cb8a35c2114a9ce803060b858c0e65ef6011228b4a6e78bcab93954f"
 
-	db, err := gorm.Open("postgres",
-		"host="+host+
-			" port="+port+
-			" user="+user+
-			" dbname="+dbname+
-			" sslmode=disable password="+password)
+	db, err := gorm.Open("postgres",connection)
 
 	if err != nil {
 		log.Fatal(err)
