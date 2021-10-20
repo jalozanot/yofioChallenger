826512c5ec6c5024e5f8c071e0dd058c6d58ff1c
jesus lozano <jalozanot@gmail.com>
2021-10-19 19:49:43 -0500

    ajuste credenciales bd

--- infrastructure/database_client/client_postgres.go
+++ infrastructure/database_client/client_postgres.go
@@ -1,21 +1,20 @@
 package database_client
 
 import (
-
-	"yofio/domain/model"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"log"
+	"yofio/domain/model"
 )
 
 func GetConnectionPostgres() *gorm.DB {
 
 
-	host := "127.0.0.1"
+	host := "ec2-174-129-229-106.compute-1.amazonaws.com"
 	port := "5432"
-	dbname := "test"
-	user := "postgres"
-	password := "postgres"
+	dbname := "d2m2hsofvodvak"
+	user := "wbqenwzebxlhed"
+	password := "78b52cbec481afe317aeaf4ecb9356a1969c1ec156bdad3dc2c451ab1d91b74f"
 
 	db, err := gorm.Open("postgres",
 		"host="+host+
